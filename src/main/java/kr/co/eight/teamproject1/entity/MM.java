64fae51e6eccdfdc359852c2af2b45e267549e33
djkkk <future1develope@kakao.com>
2022-01-21 08:37:48 +0900

    MM commit

---
+++ src/main/java/kr/co/eight/teamproject1/entity/MM.java
@@ -0,0 +1,23 @@
+package kr.co.eight.teamproject1.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MM extends BaseEntity{
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long  MM_NUM; //INT PRIMARY KEY,
+    private String MM_NAME; //VARCHAR(15) NOT NULL,
+    private String MM_TEAMNAME; // VARCHAR(30) NOT NULL
+}
---
+++ src/main/java/kr/co/eight/teamproject1/entity/Teammember.java
@@ -0,0 +1,5 @@
+package kr.co.eight.teamproject1.entity;
+
+public class Teammember {
+
+}
