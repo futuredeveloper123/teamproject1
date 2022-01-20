a80b7b142bd6555d08de904ae0556ccc2790fbd5
whwnsrb <whwnsrb2442@naver.com>
2022-01-20 19:01:23 +0900

    TLqkfusdk

--- src/main/java/kr/co/eight/teamproject1/dto/BoardVO.java
+++ src/main/java/kr/co/eight/teamproject1/dto/BoardVO.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BoardVO {
+ public class BoardVO {
  private int BOARD_NUMBER;
  private String BOARD_TITLE;
  private String  BOARD_CONTENT;
